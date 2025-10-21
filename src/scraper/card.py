37d9b075f8ad2111733cb8d2c0f0196d888d6d39
Amaury <88397823+Haloyuyu@users.noreply.github.com>
2025-10-22 00:57:56 +0200

    Changed the order of dict elements in the Card class

--- src/scraper/card.py
+++ src/scraper/card.py
@@ -8,17 +8,17 @@ class Card:
     card_id: Optional[str] = None
     set_name: Optional[str] = None
     rarity: Optional[str] = None
+    color: Optional[str] = None
     card_type: Optional[str] = None
     cost: Optional[str] = None
     life: Optional[str] = None
     power: Optional[str] = None
-    image_url: Optional[str] = None
     attribute: Optional[str] = None
     counter: Optional[str] = None
-    block: Optional[str] = None
-    color: Optional[str] = None
     effect: Optional[str] = None
     feature: Optional[str] = None
+    block: Optional[str] = None
+    image_url: Optional[str] = None
 
     def to_dict(self):
         return asdict(self)
